2986f045307a6eaac61fe62e61abd66840b97b93
Julian <thejulianjuko@gmail.com>
2025-06-05 17:56:23 +0200

    Update measurementRequests.ts

--- src/api/measurementRequests.ts
+++ src/api/measurementRequests.ts
@@ -20,6 +20,7 @@ async function deleteMeasurement(id: string): Promise<void> {
 
 /**
  * Import measurement owned by another user
+ * @param id - The id of the measurement to import
  * @returns The measurement information
  */
 async function importMeasurement(id: string): Promise<Measurement> {
@@ -32,6 +33,7 @@ async function importMeasurement(id: string): Promise<Measurement> {
 
 /**
  * Remove subscription to an imported measurement
+ * @param id - The id of the measurement to remove
  */
 async function removeImportedMeasurement(id: string): Promise<void> {
 	await axiosClient.delete<Measurement>(`/measurements/imported/${id}`);
