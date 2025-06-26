1403ef2cdbf5df016a6145159b6487771c53eac9
AlexM2801 <am214387@gmail.com>
2025-06-26 21:00:41 +0200

    Implement dummy validation screen

--- src/screens/ValidationScreen.tsx
+++ src/screens/ValidationScreen.tsx
@@ -4,6 +4,9 @@ import { FC } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import SecondaryHeader from '../components/SecondaryHeader';
 import { useTranslation } from 'react-i18next';
+import { ScrollView, Text, TextInput, View } from 'react-native';
+import Center from '../components/Center';
+import Button from '../components/Button';
 
 type ValidationScreenNavigationProp = NativeStackNavigationProp<
 	RootStackParamList,
@@ -19,12 +22,82 @@ const ValidationScreen: FC<ValidationScreenProps> = (
 ) => {
 	const { t } = useTranslation();
 
+	const bier = [
+		{
+			name: 'G',
+			default: '0',
+			unit: 'dB',
+		},
+		{
+			name: 'C50',
+			default: '0',
+			unit: 'dB',
+		},
+		{
+			name: 'C80',
+			default: '0',
+			unit: 'dB',
+		},
+		{
+			name: 'RT60',
+			default: '0',
+			unit: 'dB',
+		},
+		{
+			name: 'D50',
+			default: '0',
+			unit: '%',
+		},
+	];
+
 	return (
 		<SafeAreaView className="flex-1 bg-background">
 			<SecondaryHeader
 				title={t('validation')}
 				onBack={() => props.navigation.pop()}
 			/>
+			<ScrollView className="m-2">
+				<View className="flex-row">
+					<View className="flex-1">
+						<Text className="text-center text-lg">
+							{t('value')}
+						</Text>
+					</View>
+					<View className="flex-1">
+						<Text className="text-center text-lg">
+							{t('tolerances')}
+						</Text>
+					</View>
+				</View>
+
+				{bier.map((item, index) => {
+					return (
+						<View
+							key={index}
+							className="flex-row items-center justify-between p-5 my-2"
+						>
+							<Text className="text-base flex-1">
+								{item.name}
+							</Text>
+							<TextInput
+								value={item.default}
+								className="bg-secondary w-40 p-2 rounded-md"
+							/>
+							<Text className="text-base w-8 text-right ">
+								{item.unit}
+							</Text>
+						</View>
+					);
+				})}
+
+				<Center>
+					<Button
+						label={t('validate')}
+						onPress={() => {}}
+						expand={false}
+					/>
+				</Center>
+			</ScrollView>
 		</SafeAreaView>
 	);
 };
